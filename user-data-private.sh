90ca61259fb1aa55bf2fd2c12ef6eadbbcc08985
JLCodeSource <JLCodeSource@gmail.com>
2020-01-24 05:37:33 +0000

    updated private user data with rke
    Signed-off-by: JLCodeSource <JLCodeSource@gmail.com>

--- user-data-private.sh
+++ user-data-private.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
+
+# Update
+# NB - needed to handle grub issue
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install docker.io
+
+# Install tools
+sudo apt-get -y install docker.io jq
+
+# Download rke K8s installer
+curl -s https://api.github.com/repos/rancher/rke/releases/latest | jq -r ".assets[] | select(.name | test(\"rke_linux-amd64\")) | .browser_download_url"
+curl -L $(cat latest.txt) --output rke_linux-amd 
+
+# Install rke K8s installer
+sudo chmod 755 rke_linux-amd
+sudo chown root:root rke_linux-amd
+sudo mv rke_linux-amd /usr/local/bin/rke
 
 
